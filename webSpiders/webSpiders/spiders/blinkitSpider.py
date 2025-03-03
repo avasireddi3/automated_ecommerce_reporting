f4eff95f2b14daf2e501f1b3b2afdfe990eebfb5
Aditya Vasireddi <avasireddi3@gmail.com>
2025-03-03 16:14:19 +0530

    infinite scroll blinkit and parse listings for idli rava query

--- webSpiders/webSpiders/spiders/blinkitSpider.py
+++ webSpiders/webSpiders/spiders/blinkitSpider.py
@@ -1,3 +1,4 @@
+import json
 import re
 import scrapy
 from bs4 import BeautifulSoup
@@ -15,14 +16,13 @@ class BlinkitSpider(scrapy.Spider):
         playwright_include_page = True,
         playwright_page_methods = [
             PageMethod("wait_for_selector", "input[placeholder='search delivery location']"),
-            PageMethod("click", "input[placeholder='search delivery location']"),
-            PageMethod("fill","input[placeholder='search delivery location']","Kammana"),
-            PageMethod("click", "input[placeholder='search delivery location']"),
+            PageMethod("wait_for_timeout", 100),
+            PageMethod("fill","input[placeholder='search delivery location']","Kammanahalli"),
+            PageMethod("wait_for_timeout", 100),
             PageMethod("fill", "input[placeholder='search delivery location']", ""),
-            PageMethod("click", "input[placeholder='search delivery location']"),
-            PageMethod("wait_for_timeout",1000),
-            PageMethod("fill", "input[placeholder='search delivery location']", "Kammana"),
-            PageMethod("wait_for_timeout", 1000),
+            PageMethod("wait_for_timeout", 100),
+            PageMethod("fill", "input[placeholder='search delivery location']", "Kammanahalli"),
+            PageMethod("wait_for_timeout", 100),
             PageMethod("wait_for_selector", "div[class='address-container-v1']"),
             PageMethod("click", "div[class='LocationSearchList__LocationListContainer-sc-93rfr7-0 lcVvPT']"),
             # execute the scroll script
@@ -30,13 +30,44 @@ class BlinkitSpider(scrapy.Spider):
                        "for (let i = 0; i < 8; i++) setTimeout(() => window.scrollTo(0, document.body.scrollHeight), i * 2000);"),
             # wait for 30 seconds
             PageMethod("wait_for_timeout", 15000)
-
-
             ]
         ))
 
     async def parse(self, response):
         soup = BeautifulSoup(response.text,'lxml')
         prettyHTML = soup.prettify()
-        with open("test_html.txt","w") as f:
-            f.write(prettyHTML)
+
+        # data = soup.find("script",string=re.compile("window.grofers*")).text
+        # data = data.split("window.grofers = {};")[1]
+        # data = data.split("{",maxsplit=1)[1]
+        # data = data.split("};",maxsplit=1)[0]
+        # data = dict(data)
+        # data = "{" + data + "}"
+
+
+        products = soup.find_all("div",attrs={"class":"Product__UpdatedContentContainer-sc-11dk8zk-7 bekgjj"})
+        for product in products:
+            name = product.findChild("div",attrs={"class":"Product__UpdatedTitle-sc-11dk8zk-9 hxWnoO"}).text
+            try:
+                quantity = product.findChild("span", attrs={"class": "bff_variant_text_only plp-product__quantity--box"}).text
+            except:
+                try:
+                    quantity = product.findChild("div", attrs={"class": "plp-product__quantity--box"}).text
+                except:
+                    quantity=None
+            price_tag = product.findChild("div", attrs={"class": "Product__UpdatedPriceAndAtcContainer-sc-11dk8zk-10 ljxcbQ"})
+            prices = []
+            for child in price_tag.children:
+                for price in child.children:
+                    prices.append(price.text)
+            display_price = prices[0]
+            if "₹" in prices[1]:
+                original_price = prices[1]
+            else:
+                original_price = None
+            yield {
+                "name":name,
+                "quantity":quantity,
+                "display_price":display_price,
+                "original_price":original_price
+            }
